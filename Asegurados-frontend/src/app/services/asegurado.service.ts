15b007db12115ddd5bbd3009c6ff136db5a8cf72
Joel <joelstif00@gmail.com>
2025-01-15 20:38:36 -0500

    Se creo el servicio

---
+++ Asegurados-frontend/src/app/services/asegurado.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Asegurado } from '../models/asegurado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AseguradoService {
+  private apiUrl = 'http://localhost:5043/api/asegurados';
+
+  constructor(private http: HttpClient) { }
+
+  getAsegurados(page: number, pageSize: number): Observable<Asegurado[]> {
+    return this.http.get<Asegurado[]>(`${this.apiUrl}?page=${page}&pageSize=${pageSize}`);
+  }
+
+  getAseguradoById(id: number): Observable<Asegurado> {
+    return this.http.get<Asegurado>(`${this.apiUrl}/${id}`);
+  }
+
+  createAsegurado(asegurado: Asegurado): Observable<Asegurado> {
+    return this.http.post<Asegurado>(this.apiUrl, asegurado);
+  }
+
+  updateAsegurado(id: number, asegurado: Asegurado): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, asegurado);
+  }
+
+  deleteAsegurado(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
