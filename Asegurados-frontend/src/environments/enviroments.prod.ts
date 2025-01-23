7c52676325d92d72c0def8dd25a5d619b0292593
Joel <joelstif00@gmail.com>
2025-01-23 11:20:36 -0500

    Se configuro el despliegue del frontend para firebase y se actualizo .gitignore

---
+++ Asegurados-frontend/src/environments/enviroments.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiKey: "AIzaSyAxbF2x4sAm89kN4Q8wIkW232ghcnKJqaU",
+    authDomain: "asegurados-8885d.firebaseapp.com",
+    projectId: "asegurados-8885d",
+    storageBucket: "asegurados-8885d.firebasestorage.app",
+    messagingSenderId: "824199259934",
+    appId: "1:824199259934:web:330ce0fb8d4a65893f8b0f"
+}
--- Asegurados-frontend/src/main.ts
+++ Asegurados-frontend/src/main.ts
@@ -4,10 +4,14 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { initializeApp } from "firebase/app";
+import { environment } from "./environments/enviroments.prod"
 
-bootstrapApplication(AppComponent,{
+const app = initializeApp(environment);
+
+bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(RouterModule.forRoot(routes, { useHash: true })),
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(RouterModule.forRoot(routes)),
+    importProvidersFrom(HttpClientModule)
   ],
 }).catch((err) => console.error(err));
